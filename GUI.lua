9fa84586df4ea8ac375e678aa3a19f6994edc978
NinR3227 <161265005+NinR3227@users.noreply.github.com>
2025-08-12 20:14:56 +0800

    v1

--- GUI.lua
+++ GUI.lua
@@ -1,1 +1,17 @@
+-- GUI.lua
+local GUI = {}
 
+function GUI:Init(Config)
+    local screenGui = Instance.new("ScreenGui", game.Players.LocalPlayer:WaitForChild("PlayerGui"))
+    screenGui.Name = "VnsHub"
+
+    local mainFrame = Instance.new("Frame", screenGui)
+    mainFrame.Size = UDim2.new(0, 400, 0, 300)
+    mainFrame.Position = UDim2.new(0.5, -200, 0.5, -150)
+    mainFrame.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+    mainFrame.Name = "MainFrame"
+
+    self.MainFrame = mainFrame
+end
+
+return GUI

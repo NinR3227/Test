8ecc4d9164e716b03f6aff9c459a76b711aec7aa
NinR3227 <161265005+NinR3227@users.noreply.github.com>
2025-08-12 21:06:30 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,2 @@
--- main.lua
-local Config = loadstring(game:HttpGet("https://raw.githubusercontent.com/NinR3227/VnsHub/main/Config.lua"))()
-local GUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/NinR3227/VnsHub/main/GUI.lua"))()
-local Automation = loadstring(game:HttpGet("https://raw.githubusercontent.com/NinR3227/VnsHub/main/Automation.lua"))()
-
-GUI:Init(Config)
-Automation:Init(GUI.MainFrame, Config)
+local GUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/NinR3227/Test/main/GUI.lua"))()
+GUI()

8b4daaa54658ad9eabfd023b1abe9c52486edac4
NinR3227 <161265005+NinR3227@users.noreply.github.com>
2025-08-12 21:21:31 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,14 +1,11 @@
--- main.lua
 local success, result = pcall(function()
     print("🔄 Loading GUI module...")
 
     local GUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/NinR3227/Test/main/GUI.lua"))()
-    
     if typeof(GUI) == "function" then
         GUI()
-        print("✅ GUI loaded and executed successfully.")
     else
-        warn("⚠️ GUI module did not return a function.")
+        warn("⚠️ GUI.lua did not return a function.")
     end
 end)
 

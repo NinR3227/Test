43a38572c6b2ecdb4baa9ac5e095109ba917f9ec
NinR3227 <161265005+NinR3227@users.noreply.github.com>
2025-08-12 20:09:05 +0800

    v1

--- config.lua
+++ config.lua
@@ -1,1 +1,13 @@
-
+-- Config.lua
+return {
+    Font = Enum.Font.SourceSansBold,
+    TextSize = 18,
+    ButtonColor = {
+        Selected = Color3.fromRGB(120, 120, 0),
+        Default = Color3.fromRGB(80, 80, 80),
+        Action = Color3.fromRGB(0, 120, 0),
+        All = Color3.fromRGB(120, 120, 0)
+    },
+    TextColor = Color3.new(1, 1, 1),
+    Delay = 0.2
+}
